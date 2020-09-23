3ec023b8259e4ced7fcac9aaa91a06f1ec7a67c3
Patricia Arnedo <paaarnedo@gmail.com>
2020-09-23 07:00:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,7 @@ end
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
   pioneer = {
-    :name = 'Grace Hopper'
+    :name => 'Grace Hopper'
   }
   
 end
@@ -25,7 +25,7 @@ def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
   
   id_hash = {
-    :id = number
+    :id => number
   }
   
 end
