60137c44c7858795d6cb19bb246a59a1d2ff0083
Patricia Arnedo <paaarnedo@gmail.com>
2020-09-23 07:02:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -7,8 +7,8 @@ end
 def my_hash
   # return a valid hash with any key/value pair of your choice
   my_hash = {
-    me => "student"
-    :you => "teacher"
+    me => "student",
+    :you => "teacher",
     them: "peers"
   }
   
